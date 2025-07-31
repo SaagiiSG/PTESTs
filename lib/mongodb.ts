40baff5d99020c9822da24e345cd485c8263e58e
Saranochir Sereglen <saranochirsereglen@Saranochirs-Laptop.local>
2025-07-31 09:41:21 +0800

    mongodb connection issue 3

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -23,7 +23,6 @@ export async function connectMongoose() {
       maxPoolSize: 10, // Maintain up to 10 socket connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      bufferMaxEntries: 0, // Disable mongoose buffering
       bufferCommands: false, // Disable mongoose buffering
       connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
       retryWrites: true,
